be62a9a33e5c93173364df243b9e0f0b0615f4fa
liangscc <liangscc@163.com>
2025-07-24 06:25:49 +0800

    init

---
+++ App.vue
@@ -0,0 +1,28 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			 // 检测更新
+			 // 网络监听
+			 // 初始化数据
+		},
+		onShow: function() {
+			console.log('App Show') 
+			// 恢复播放
+		},
+		onHide: function() {
+			console.log('App Hide')
+			// 停在播放
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 官方 css 库 */
+	@import "./common/uni.css";
+	/* iconfont */
+	@import "./common/iconfont/iconfont.css";
+	/* 动画库 */
+	@import "./common/animate.min.css";
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}111</text>
+		</view>
+		<view class="text-area">
+			<view class="animate__animated" hover-class="animate__shakeX" style="border: 1rpx solid red; padding: 10rpx;">
+				<text class="iconfont icon-a-11-xianghuozhuizong" style="color: red; font-size: 24px;"></text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
